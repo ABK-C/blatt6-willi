0b4418ea5cc7dd2e875ab5ebb54aabed33974e11
repl.it user <replituser@example.com>
2020-06-17 19:12:47 +0000

    Blatt 6

--- Vollzylinder.cc
+++ Vollzylinder.cc
@@ -9,11 +9,12 @@ Vollzylinder::Vollzylinder(double nr, double nl) {
 }
 
 Vektor Vollzylinder::punkt() {
-  double R = 0;
+  double R = r_;
   double phi = 2 * M_PI * rand() / (double)RAND_MAX;
   double z = l_ * (rand() / (double)RAND_MAX - 0.5);
 
-  double x = 0;
-  double y = 0;
+ 
+  double x = r_ * cos(phi);
+  double y = r_ * sin(phi);
   return Vektor(x, y, z);
 };
--- traegheit.cc
+++ traegheit.cc
@@ -6,31 +6,33 @@
 
 int main() {
   const int N = 10000;     // Anzahl Integrationspunkte
-  const double M = 1;      // Masse des Zylindermantels
+  const double M = 2;      // Masse des Zylindermantels
   const double ZM_R = 1.0; // Radius der Zylindermantels
   const double ZM_L = 1.0; // Laenge des Zylindermantels
 
   Vektor a; // Punkt auf der Rotationsachse
-  std::cout << "Aufpunkt:";
+  std::cout << "Auflagepunkt:";
   std::cin >> a;
   Vektor u; // Richtung der Rotationsachse
   std::cout << "Richtung:";
   std::cin >> u;
 
-  std::unique_ptr<Zylindermantel> zm(new Zylindermantel(ZM_R, ZM_L));
+  std::unique_ptr<Vollzylinder> new Vollzylinder(ZM_R, ZM_L));
 
   double J = 0;     // Massentraegheitsmoment
   double m = M / N; // Masse eines Massenpunktes
   for (int i = 0; i < N; ++i) {
     Vektor x = zm->punkt();
+    Vektor d = x - a;
     // Abstand Punkt x und Gerade a + t*u
+    Vektor n = d.kreuz(u);
     // Vektor n = ...;//Normalenvektor x-a kreuz u
-    double r = 0; //|n|/|u|
-    // std::cout << x << " :" << r << std::endl;
+    double r = n.betrag()/u.betrag();
+    std::cout << x << " :" << r << std::endl;
     // addiere Beitrag des Massenpunktes zum Traegheitsmoment
-    J += m * r * r;
+    J += 0.5*m * r * r;
   }
-  std::cout << "Massentraegheitsmoment fuer einen Zylindermantel"
+  std::cout << "Massentraegheitsmoment fuer einen Vollzylinder"
             << " mit a = " << a << " und u = " << u << ": " << J << std::endl;
             
   return 0;
